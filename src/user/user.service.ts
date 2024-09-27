e01db280dd7b33083145874d352ef9d79ecc4b47
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-09-27 12:43:27 +0530

    added hash when user update the info

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/schemas/user.schema';
 import { UpdateUseInfoDto } from './dto/updateUser.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -14,12 +15,18 @@ export class UserService {
     id: string,
     updateUserDto: UpdateUseInfoDto,
   ): Promise<User> {
+    if (updateUserDto.password) {
+      const salt = await bcrypt.genSalt(10);
+      updateUserDto.password = await bcrypt.hash(updateUserDto.password, salt);
+    }
     const user = await this.userModel.findByIdAndUpdate(id, updateUserDto, {
       new: true,
     });
+
     if (!user) {
       throw new NotFoundException('User not found');
     }
+    user.password = undefined;
     return user;
   }
 }
